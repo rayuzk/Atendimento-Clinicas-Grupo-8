a682e7ad4fee296966f32ab80a9cff1c36d51d0e
Marcelabalecrim <marcelab.alecrim@gmail.com>
2026-06-07 11:28:33 -0300

    Update paciente.py

--- clinica/model/paciente.py
+++ clinica/model/paciente.py
@@ -1,4 +1,15 @@
+from model.pessoa import Pessoa
+
 class Paciente(Pessoa):
+
     def __init__(self, idade, nome, celular, cpf):
         super().__init__(nome, celular, cpf)
-        self.idade = idade
+        self.__idade = idade
+
+    @property
+    def idade(self):
+        return self.__idade
+
+    @idade.setter
+    def idade(self, idade):
+        self.__idade = idade
