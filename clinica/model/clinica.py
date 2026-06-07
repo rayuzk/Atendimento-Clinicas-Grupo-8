9e9649d5a86455fc57b6795fc147ab0ada68b351
Marcelabalecrim <marcelab.alecrim@gmail.com>
2026-06-07 11:26:39 -0300

    Update clinica.py

--- clinica/model/clinica.py
+++ clinica/model/clinica.py
@@ -1,5 +1,26 @@
 class Clinica():
+
     def __init__(self, nome, cidade, descricao):
-        self.nome = nome
-        self.cidade = cidade
-        self.descricao = descricao
+        self.__nome = nome
+        self.__cidade = cidade
+        self.__descricao = descricao
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def cidade(self):
+        return self.__cidade
+
+    @cidade.setter
+    def cidade(self, cidade):
+        self.__cidade = cidade
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, descricao):
+        self.__descricao = descricao
