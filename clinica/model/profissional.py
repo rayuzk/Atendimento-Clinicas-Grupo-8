476d893824cbed9e5c7d2475433c09fbe41ad84c
Marcelabalecrim <marcelab.alecrim@gmail.com>
2026-06-07 11:30:22 -0300

    Update profissional.py

--- clinica/model/profissional.py
+++ clinica/model/profissional.py
@@ -1,5 +1,20 @@
+from model.pessoa import Pessoa
+
 class Profissional(Pessoa):
+
     def __init__(self, especialidade, registro, nome, celular, cpf):
         super().__init__(nome, celular, cpf)
-        self.especialidade = especialidade
-        self.registro = registro
+        self.__especialidade = especialidade
+        self.__registro = registro
+
+    @property
+    def especialidade(self):
+        return self.__especialidade
+
+    @especialidade.setter
+    def especialidade(self, especialidade):
+        self.__especialidade = especialidade
+
+    @property
+    def registro(self):
+        return self.__registro
