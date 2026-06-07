b18007fa95a4077b237d4c86f6d9b2f9b8ff0337
Marcelabalecrim <marcelab.alecrim@gmail.com>
2026-06-07 11:27:42 -0300

    Update pessoa.py

--- clinica/model/pessoa.py
+++ clinica/model/pessoa.py
@@ -1,5 +1,26 @@
 class Pessoa():
+
     def __init__(self, nome, celular, cpf):
-        self.nome = nome
-        self.celular = celular
-        self.cpf = cpf 
+        self.__nome = nome
+        self.__celular = celular
+        self.__cpf = cpf
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def celular(self):
+        return self.__celular
+
+    @celular.setter
+    def celular(self, celular):
+        self.__celular = celular
+
+    @property
+    def cpf(self):
+        return self.__cpf
