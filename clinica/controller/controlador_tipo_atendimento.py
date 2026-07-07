e4c9b1bc16698eb36891990073177b35fda9a55f
Marcelabalecrim <marcelab.alecrim@gmail.com>
2026-07-07 08:17:03 -0300

    Update controlador_tipo_atendimento.py

--- clinica/controller/controlador_tipo_atendimento.py
+++ clinica/controller/controlador_tipo_atendimento.py
@@ -1,10 +1,13 @@
 from model.tipo_atendimento import TipoAtendimento
+from dao.tipo_atendimento_dao import TipoAtendimentoDAO
+
 
 class ControladorTipoAtendimento:
 
     def __init__(self, controlador_sistema):
         self.controlador_sistema = controlador_sistema
-        self.tipos_atendimento = []
+        self.__tipo_atendimento_dao = TipoAtendimentoDAO()
+        self.tipos_atendimento = self.__tipo_atendimento_dao.carregar()
 
     def incluir_tipo_atendimento(self, descricao):
         if not descricao:
@@ -12,6 +15,7 @@ class ControladorTipoAtendimento:
 
         tipo_atendimento = TipoAtendimento(descricao)
         self.tipos_atendimento.append(tipo_atendimento)
+        self.__tipo_atendimento_dao.salvar(self.tipos_atendimento)
         return tipo_atendimento
 
     def listar_tipos_atendimento(self):
@@ -21,6 +25,7 @@ class ControladorTipoAtendimento:
         for tipo_atendimento in self.tipos_atendimento:
             if tipo_atendimento.descricao == descricao:
                 self.tipos_atendimento.remove(tipo_atendimento)
+                self.__tipo_atendimento_dao.salvar(self.tipos_atendimento)
                 return
         raise ValueError("Tipo de atendimento não encontrado.")
 
@@ -28,6 +33,7 @@ class ControladorTipoAtendimento:
         for tipo_atendimento in self.tipos_atendimento:
             if tipo_atendimento.descricao == descricao:
                 tipo_atendimento.descricao = nova_descricao
+                self.__tipo_atendimento_dao.salvar(self.tipos_atendimento)
                 return
         raise ValueError("Tipo de atendimento não encontrado.")
 
